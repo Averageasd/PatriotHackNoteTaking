596f29896d5462e9d7f9e83652e74e009a7b8ec6
Nguyen Huynh <nhuynh9497@gmail.com>
2023-10-07 11:32:32 -0400

    use env file to hide api keys

--- upload.py
+++ upload.py
@@ -3,14 +3,17 @@ import os.path
 import boto3
 import api_constant
 import mp4_to_mp3_converter
+from dotenv import load_dotenv
+
 
 
 class Upload:
+    load_dotenv()
     upload_service = boto3.client(
         's3',
-        aws_session_token=api_constant.aws_session_token,
-        aws_access_key_id=api_constant.aws_access_key_id,
-        aws_secret_access_key=api_constant.aws_secret_access_key
+        aws_session_token=os.getenv('aws_session_token'),
+        aws_access_key_id=os.getenv('aws_access_key_id'),
+        aws_secret_access_key=os.getenv('aws_secret_access_key')
     )
 
     @staticmethod
